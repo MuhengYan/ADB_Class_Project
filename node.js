05651d3ce99218154139b278b3e4bb9c038deeef
YZk67 <43705015+YZk67@users.noreply.github.com>
2020-04-19 13:14:06 -0400

    Update node.js

--- node.js
+++ node.js
@@ -1,1 +1,68 @@
-To be continue
+{"_id":"United Kingdom","sum":129,"avg":2.9318181818181817}
+{"_id":"Egypt","sum":49,"avg":2.5789473684210527}
+{"_id":"India","sum":42,"avg":2.8}
+{"_id":"Peru","sum":42,"avg":3.8181818181818183}
+{"_id":"Germany","sum":37,"avg":1.5416666666666667}
+{"_id":"United States","sum":37,"avg":1.4230769230769231}
+{"_id":"West Bank and Gaza Strip","sum":36,"avg":1.7142857142857142}
+{"_id":"Lebanon","sum":36,"avg":1.6363636363636365}
+{"_id":"Cambodia","sum":33,"avg":1.375}
+{"_id":"Turkey","sum":29,"avg":1.7058823529411764}
+{"_id":"Algeria","sum":23,"avg":1.9166666666666667}
+{"_id":"Somalia","sum":23,"avg":5.75}
+{"_id":"Colombia","sum":21,"avg":3.0}
+{"_id":"Israel","sum":21,"avg":1.4}
+{"_id":"South Africa","sum":18,"avg":1.3846153846153846}
+{"_id":"Spain","sum":14,"avg":2.3333333333333335}
+{"_id":"Chile","sum":13,"avg":6.5}
+{"_id":"France","sum":12,"avg":1.2}
+{"_id":"Austria","sum":11,"avg":2.75}
+{"_id":"Italy","sum":10,"avg":1.4285714285714286}
+{"_id":"Pakistan","sum":8,"avg":1.6}
+{"_id":"Philippines","sum":8,"avg":1.3333333333333333}
+{"_id":"Sri Lanka","sum":7,"avg":1.4}
+{"_id":"Japan","sum":7,"avg":3.5}
+{"_id":"Yemen","sum":6,"avg":1.5}
+{"_id":"Russia","sum":4,"avg":1.0}
+{"_id":"Venezuela","sum":4,"avg":4.0}
+{"_id":"Angola","sum":4,"avg":1.0}
+{"_id":"Iran","sum":4,"avg":2.0}
+{"_id":"Togo","sum":3,"avg":3.0}
+{"_id":"Afghanistan","sum":3,"avg":1.0}
+{"_id":"Iraq","sum":3,"avg":1.0}
+{"_id":"Switzerland","sum":3,"avg":1.5}
+{"_id":"Nicaragua","sum":3,"avg":1.0}
+{"_id":"Panama","sum":2,"avg":1.0}
+{"_id":"Denmark","sum":2,"avg":1.0}
+{"_id":"Costa Rica","sum":2,"avg":2.0}
+{"_id":"Bolivia","sum":2,"avg":1.0}
+{"_id":"Papua New Guinea","sum":2,"avg":1.0}
+{"_id":"Myanmar","sum":2,"avg":1.0}
+{"_id":"Niger","sum":2,"avg":2.0}
+{"_id":"Greece","sum":2,"avg":2.0}
+{"_id":"Thailand","sum":2,"avg":1.0}
+{"_id":"Yugoslavia","sum":2,"avg":1.0}
+{"_id":"Croatia","sum":2,"avg":1.0}
+{"_id":"Paraguay","sum":1,"avg":1.0}
+{"_id":"El Salvador","sum":1,"avg":1.0}
+{"_id":"Ethiopia","sum":1,"avg":1.0}
+{"_id":"Estonia","sum":1,"avg":1.0}
+{"_id":"Ecuador","sum":1,"avg":1.0}
+{"_id":"Georgia","sum":1,"avg":1.0}
+{"_id":"Sierra Leone","sum":1,"avg":1.0}
+{"_id":"Haiti","sum":1,"avg":1.0}
+{"_id":"Kuwait","sum":1,"avg":1.0}
+{"_id":"Norway","sum":1,"avg":1.0}
+{"_id":"China","sum":1,"avg":1.0}
+{"_id":"Latvia","sum":1,"avg":1.0}
+{"_id":"Tanzania","sum":1,"avg":1.0}
+{"_id":"Azerbaijan","sum":1,"avg":1.0}
+{"_id":"Tajikistan","sum":1,"avg":1.0}
+{"_id":"Belarus","sum":1,"avg":1.0}
+{"_id":"Tunisia","sum":1,"avg":1.0}
+{"_id":"Uganda","sum":1,"avg":1.0}
+{"_id":"Argentina","sum":1,"avg":1.0}
+{"_id":"Rwanda","sum":1,"avg":1.0}
+{"_id":"Guatemala","sum":1,"avg":1.0}
+{"_id":"Netherlands","sum":1,"avg":1.0}
+{"_id":"Romania","sum":1,"avg":1.0}
